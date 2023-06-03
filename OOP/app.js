6de89335127f1d0e23af528571f6e5a2fbfbacad
bronja9 <otraditi@gmail.com>
2023-06-03 11:16:34 +0200

    oop

--- OOP/app.js
+++ OOP/app.js
@@ -1,1 +1,12 @@
-console.log("lelo");
+//encapsulation
+
+let employee = {
+  baseSalary: 30000,
+  overtime: 10,
+  rate: 20,
+  getWage() {
+    return this.baseSalary + this.overtime * this.rate;
+  },
+};
+
+console.log(employee.getWage());
