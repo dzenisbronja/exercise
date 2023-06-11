3e94db3e2ea49badb4d24077c7657d3368aa70fd
bronja9 <otraditi@gmail.com>
2023-06-11 19:08:48 +0200

    nums

--- float, int, num/app.js
+++ float, int, num/app.js
@@ -6,13 +6,19 @@ let myFloat = 123.345432;
 let mySrtingFloat = "322.243432.43.32";
 let cost = "12.99 per box";
 
-/////////////////////////////////////////////////////////// myString
+// ///////////////////////////////////////////////////////// myString
 // console.log(typeof mySrting); //string
-// console.log(typeof parseFloat(mySrting)); //number
-// console.log(typeof parseInt(mySrting)); //number
-// console.log(typeof Number(mySrting)); //number
 
-///////////////////////////////////////////////////////// address
+// console.log(typeof parseFloat(mySrting));
+// console.log(parseFloat(mySrting)); // 456 =>number
+
+// console.log(typeof parseInt(mySrting));
+// console.log(parseInt(mySrting)); // 456 =>number
+
+// console.log(typeof Number(mySrting));
+// console.log(Number(mySrting)); // 456 =>number
+
+// /////////////////////////////////////////////////////// address
 // console.log(typeof address); //string
 
 // console.log(typeof parseFloat(address)); //ako se na prvom mestu nadje slovo NaN
@@ -24,7 +30,7 @@ let cost = "12.99 per box";
 // console.log(typeof Number(address));
 // console.log(Number(address)); //NaN  =>number
 
-/////////////////////////////////////////////////////// myFloat
+// ///////////////////////////////////////////////////// myFloat
 // console.log(typeof myFloat); //number
 
 // console.log(typeof parseFloat(myFloat));
@@ -36,7 +42,7 @@ let cost = "12.99 per box";
 // console.log(typeof Number(myFloat));
 // console.log(Number(myFloat)); //123.345432 =>number
 
-//////////////////////////////////////////////////// mySrtingFloat
+// ////////////////////////////////////////////////// mySrtingFloat
 
 // console.log(typeof mySrtingFloat); //string
 
@@ -46,18 +52,18 @@ let cost = "12.99 per box";
 // console.log(typeof parseInt(mySrtingFloat)); //ako pocne sa slovom NaN, float takodje
 // console.log(parseInt(mySrtingFloat)); //322 =>number
 
-// console.log(typeof Number(mySrtingFloat));
+// console.log(typeof Number(mySrtingFloat)); //Vratice vrednost broja float ako nema vise od jedne tacke
 // console.log(Number(mySrtingFloat)); // NaN => number
 
-////////////////////////////////////////////////// cost
+// //////////////////////////////////////////////// cost
 
-console.log(typeof cost); //string
+// console.log(typeof cost); //string
 
-console.log(typeof parseFloat(cost)); //ako stavimo slovo prvo NaN
-console.log(parseFloat(cost)); //12.99 =>number
+// console.log(typeof parseFloat(cost)); //ako stavimo slovo prvo NaN
+// console.log(parseFloat(cost)); //12.99 =>number
 
-console.log(typeof parseInt(cost)); //ako stavimo slovo prvo NaN
-console.log(parseInt(cost)); //12 =>number
+// console.log(typeof parseInt(cost)); //ako stavimo slovo prvo NaN
+// console.log(parseInt(cost)); //12 =>number
 
-console.log(typeof Number(cost));
-console.log(Number(cost)); //NaN =>number
+// console.log(typeof Number(cost));
+// console.log(Number(cost)); //NaN =>number
