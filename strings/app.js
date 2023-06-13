63279437e8fc49e9fe9707bea75d79166a169715
bronja9 <otraditi@gmail.com>
2023-06-13 21:12:46 +0200

    s

---
+++ strings/app.js
@@ -0,0 +1,9 @@
+let ime = "sead";
+let iseceno = ime.split("");
+console.log(iseceno);
+
+iseceno.splice(1, 0, "dsadsa");
+console.log(iseceno);
+
+iseceno.splice(0, 1, "prva");
+console.log(iseceno);
