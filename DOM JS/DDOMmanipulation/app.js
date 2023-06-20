4761e8f1072786ff2667e34560dbf47859e758a3
bronja9 <otraditi@gmail.com>
2023-06-20 15:27:29 +0200

    m

--- DOM JS/DDOMmanipulation/app.js
+++ DOM JS/DDOMmanipulation/app.js
@@ -1,11 +1,21 @@
 // EVENT LISTENERS
 
-// prvi nacin je preko onclic-a, primer prvog dugmeta
+// prvi nacin je preko OCLICK-a, primer prvog dugmeta
 
-// drugi je event listener => element.addEventListener("click",function)
+// drugi je EVENT LISTENER => element.addEventListener("click",function)
 
 const buttonTwo = document.querySelector(".btn-2");
 
 buttonTwo.addEventListener("click", () => alert("dodao sam event listener"));
 // moze biti bolja praksa da se u zagradi poziva samo ime funkcije koju smo
 // prethodno napravili (callback function), a ne da se pravi na licu mesta
+
+// treci nacin je MOUSEOVER (kada hover-amo)
+
+const newBackgroundColor = document.querySelector(".box-3");
+
+function changeBgColor() {
+  newBackgroundColor.style.backgroundColor = "blue";
+}
+
+newBackgroundColor.addEventListener("mouseover", changeBgColor);
