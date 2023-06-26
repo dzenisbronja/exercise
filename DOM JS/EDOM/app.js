fa321723a6294cd25646337d5de56200dd62439d
bronja9 <otraditi@gmail.com>
2023-06-26 14:53:45 +0200

    m

--- DOM JS/EDOM/app.js
+++ DOM JS/EDOM/app.js
@@ -1,1 +1,16 @@
-console.log("dsa");
+// reveal
+
+const revealBtn = document.querySelector(".reveal-btn");
+const hiddenContent = document.querySelector(".hidden-content");
+
+function revealContent() {
+  if (hiddenContent.classList.contains("reveal-btn")) {
+    hiddenContent.classList.remove("reveal.btn");
+  } else {
+    hiddenContent.classList.add("reveal.btn");
+  }
+}
+
+revealBtn.addEventListener("click", revealContent());
+
+//ne radi, jer nisam sve prepisao
